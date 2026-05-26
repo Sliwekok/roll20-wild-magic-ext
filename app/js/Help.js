1a061cbab22403298ce91ecf3963cf7d74d18746
Sliwekok <dominiksliwek@gmail.com>
2026-05-26 15:00:22 +0200

    time

--- app/js/Help.js
+++ app/js/Help.js
@@ -1,6 +1,6 @@
 export const Help = (() => {
     async function getHelp() {
-        let message = '';
+        let message = `&{template:default} {{name=Dostepne komendy czatu}} {{help=Pokazuje tę wiadomość}} {{wild_magic=Losuje efekt z tablicy dzikiej magii (tylko GM widzi wynik)}} {{time=Pokazuje obecny czas świata}}`;
 
         return {message: message};
     }
